e55e9b0a6de6b5ea7891848a7e3c3913742d8adf
aden kesuma <adenfdfd10@gmail.com>
2023-05-12 13:36:46 +0700

    add myDesign content

---
+++ src/pages/design/index.jsx
@@ -0,0 +1,53 @@
+import { SimpleGrid, Container, Heading } from '@chakra-ui/react';
+import Layout from '../../components/Layouts/Article';
+import { WorkImage } from '@/components/Works';
+import Link from 'next/link';
+
+const Design = () => {
+    return (
+      <Layout title="MyDesign">
+        <Container maxW="2xl">
+          <Heading as="h3" fontSize={20} mb={6}>
+            My Design
+          </Heading>
+
+          <SimpleGrid columns={2} gap={4}>
+            <WorkImage src="/images/designs/design-13.png" alt="design 13" />
+            <WorkImage src="/images/designs/design-14.png" alt="design 14" />
+          </SimpleGrid>
+          <SimpleGrid columns={2} gap={4}>
+            <WorkImage src="/images/designs/design-11.png" alt="design 11" />
+            <WorkImage src="/images/designs/design-12.png" alt="design 12" />
+          </SimpleGrid>
+          <SimpleGrid columns={2} gap={4}>
+            <WorkImage src="/images/designs/design-1.png" alt="design 1" />
+            <WorkImage src="/images/designs/design-2.png" alt="design 2" />
+          </SimpleGrid>
+          <SimpleGrid columns={2} gap={4}>
+            <WorkImage src="/images/designs/design-3.png" alt="design 3" />
+            <WorkImage src="/images/designs/design-4.png" alt="design 4" />
+          </SimpleGrid>
+          <SimpleGrid columns={2} gap={4}>
+            <WorkImage src="/images/designs/design-5.png" alt="design 5" />
+            <WorkImage src="/images/designs/design-6.png" alt="design 6" />
+          </SimpleGrid>
+          <SimpleGrid columns={2} gap={4}>
+            <WorkImage src="/images/designs/design-7.png" alt="design 7" />
+            <WorkImage src="/images/designs/design-8.png" alt="design 8" />
+          </SimpleGrid>
+          <SimpleGrid columns={2} gap={4}>
+            <WorkImage src="/images/designs/design-9.png" alt="design 9" />
+            <WorkImage src="/images/designs/design-10.png" alt="design 10" />
+          </SimpleGrid>
+
+          <span style={{ color: "#19A7CE", fontWeight: '700' }}>
+            <Link href="https://www.instagram.com/adenkesuma/" target="_blank">
+              See More on Instagram{" "}
+            </Link>
+          </span>
+        </Container>
+      </Layout>
+    );
+}
+
+export default Design;
